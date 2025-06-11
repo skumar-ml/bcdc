b22ed261d9503564df585a2732d9c44cd1adcea0
yogii30 <92358002+yogii30@users.noreply.github.com>
2025-06-11 13:53:03 +0530

    updated api call for prev student

--- class_details_stripe_new.js
+++ class_details_stripe_new.js
@@ -1324,7 +1324,7 @@ function creEl(name, className, idName) {
       var $this = this;
       try {
         const data = await this.fetchData(
-          "getAllPreviousStudents/" + this.webflowMemberId
+          "getAllPreviousStudents/" + this.webflowMemberId+"/all"
         );
         console.log("Old Student Data", data);
   
