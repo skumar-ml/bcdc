cb3411db79adcad74ae0cd4df32b21b695e78b43
yogii30 <92358002+yogii30@users.noreply.github.com>
2025-07-28 15:39:09 +0530

    Update checkout_summer_bundle.js

--- checkout_summer_bundle.js
+++ checkout_summer_bundle.js
@@ -650,7 +650,7 @@ class CheckOutWebflow {
 			// Display summer session
 			this.displaySessionsData(data)
 			// Setup back button for browser and stripe checkout page
-			this.setUpBackButtonTab();
+			//this.setUpBackButtonTab();
 			// Update basic data
 			this.updateBasicData();
 			// Hide spinner 
